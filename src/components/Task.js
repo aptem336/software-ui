4fef4931ebd3b7cdf22f9d2bc722a49258f1e32e
popov <aptem336@gmail.com>
2022-05-25 12:13:46 +0800

    task form and complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,22 +2,54 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const Task = ({id}) => {
-    const [taskRenderedForm, setTaskRenderedForm] = useState();
+    const [taskDeployedForm, setTaskDeployedForm] = useState();
+    const [taskFormVariables, setTaskFormVariables] = useState();
     useEffect(() => {
-        getTaskList()
+        getTaskDeployedForm()
+        getTaskFormVariables()
     }, []);
 
-    const getTaskList = () => {
-        axios.get(`/engine-rest/task/${id}/rendered-form`)
+    const getTaskDeployedForm = () => {
+        axios.get(`/engine-rest/task/${id}/deployed-form`)
             .then((response) => {
-                setTaskRenderedForm(response.data);
+                setTaskDeployedForm(response.data);
             })
     }
+    const getTaskFormVariables = () => {
+        axios.get(`/engine-rest/task/${id}/form-variables`)
+            .then((response) => {
+                setTaskFormVariables(response.data);
+            })
+    }
+    const onChange = (event, key) => {
+        if (event.target.type === 'checkbox') {
+            taskFormVariables[key].value = event.target.checked
+        } else {
+            taskFormVariables[key].value = event.target.value
+        }
+    }
+    const complete = () => {
+        axios.post(`/engine-rest/task/${id}/complete`, {
+            variables: taskFormVariables
+        }).then()
+    }
     return (
-        <div>
-            <form dangerouslySetInnerHTML={{__html: taskRenderedForm}}/>
-            <br/>
-            {taskRenderedForm}
-        </div>
+        <form action={'/task'}>
+            {
+                taskDeployedForm?.components?.map((component) => {
+                    return <div>
+                        <label>
+                            {component?.label}<br/>
+                            <input type={component?.type}
+                                   value={taskFormVariables && taskFormVariables[component?.key]?.value}
+                                   readOnly={component?.validate?.readonly}
+                                   required={component?.validate?.required}
+                                   onChange={event => onChange(event, component.key)}/><br/>
+                        </label><br/>
+                    </div>
+                })
+            }
+            <button onClick={complete}>Завершить</button>
+        </form>
     )
 }
